5d5ea299f857aa548d166d55fa23aaf3782d46cb
Archie Estrin <archie.estrin@icloud.com>
2026-05-01 19:46:18 -0500

    add PWA support

---
+++ public/icon.sv
@@ -0,0 +1,4 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512">
+  <rect width="512" height="512" fill="#0e0e0e"/>
+  <text x="256" y="320" font-family="Georgia, serif" font-size="260" font-weight="700" fill="#ef4444" text-anchor="middle">IL</text>
+</svg>
